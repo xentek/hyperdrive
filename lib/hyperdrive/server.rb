30ad41648f30f1de76a76ad0fd4ac45d9286311c
Eric Marden <eric@xentek.net>
2014-04-17 17:48:57 -0500

    first pass at hyperdrive server
    - maps endpoints but they all return the same response and generally do
      nothing useful yet

---
+++ lib/hyperdrive/server.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+module Hyperdrive
+  class Server
+    def self.call(env)
+      server.call(env)
+    end
+
+    private
+    
+    def self.server
+      Rack::Builder.new do
+        hyperdrive.resources.each do |key, resource|
+          map resource.endpoint do
+            run lambda { |x| [200, { "Content-Type" => "text/plain" }, ["OK"]] }
+          end
+        end
+      end.to_app
+    end
+  end
+end
---
+++ spec/hyperdrive/server_spec.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+
+describe Hyperdrive::Server do
+  it "handles GET requests successfully"
+  it "handles HEAD requests successfully"
+  it "handles OPTIONS requests successfully"
+  it "handles POST requests successfully"
+  it "handles PUT requests successfully"
+  it "handles PATCH requests successfully"
+  it "handles DELETE requests successfully"
+end

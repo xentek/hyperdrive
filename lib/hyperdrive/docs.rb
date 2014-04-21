296d122640d288287bded0b60183527a7f178ff2
Justin Park <jpark3000@gmail.com>
2014-04-21 10:04:17 -0500

    refactors to decouple Hyperdrive::Docs with Hyperdrive API internals, changes specs in accordance

--- lib/hyperdrive/docs.rb
+++ lib/hyperdrive/docs.rb
@@ -3,94 +3,73 @@
 module Hyperdrive
   class Docs
     attr_reader :resources
-    attr_accessor :docs
 
     def initialize(resources)
       @resources = resources
-      @docs = ""
+    end
+
+    def output
+      out = ""
+      resources.each_value do |resource|
+        out += header(resource.name)
+        out += paragraph(resource.desc)
+        out += header("Endpoint URL", 2)
+        out += paragraph(bullet(code(resource.endpoint), 2))
+        out += header("Params", 2)
+        out += list(resource.allowed_params)
+        out += header("Filter", 2)
+        out += list(resource.filters)
+      end
+      out
     end
 
     def header(string, level = 1)
       raise ArgumentError, "Header level must be between 1 and 6." unless (1..6).cover?(level)
       header = "#" * level
-      "\n#{header} #{string}\n\n"
+      "\n\n#{header} #{string}\n\n"
     end
 
     def paragraph(string)
-      "#{string}\n"
+      "#{string}\n\n"
     end
 
     def bold(string)
       "__#{string}__"
     end
 
+    def italics(string)
+      "_#{string}_"
+    end
+
     def code(string)
       "`#{string}`"
     end
 
-    def bullet(string, nest = 0)
-      nest = " " * nest
-      "#{nest}- #{string}"
+    def bullet(string, nest = 1)
+      raise ArgumentError, "Nest level must be between 1 and 3." unless (1..3).cover?(nest)
+      nest = "  " * nest
+      "\n#{nest}- #{string}\n"
     end
 
-    def endpoint(endpoint)
-      paragraph(bullet(code(endpoint), 2))
-    end
-
-    def params(params)
-      params_list = ""
-      params.each do |k, v|
-        params_list += param_name(k)
-        params_list += param_desc(v[:desc])
-        params_list += required(v[:required])
+    def list(params)
+      list = ""
+      params.each do |key, value|
+        list += "#{bullet(bold(key), 1)}: "
+        list += paragraph(value[:desc])
+        list += "#{bullet(bold('Required'), 2)}: "
+        list += if value[:required].kind_of? Array
+                  code_options(value[:required])
+                elsif value[:required] == true
+                  code_options(Hyperdrive::Verbs.default)
+                else
+                  italics('none')
+                end
       end
-      params_list
+      list
     end
 
-    def param_name(name)
-      "#{bullet(bold(name), 2)}: "
+    def code_options(options)
+      options.map { |option| code(option) }.join(", ")
     end
-
-    def param_desc(desc)
-      paragraph(desc)
-    end
-
-    def required(required_opts)
-      default_opts = ["GET", "HEAD", "OPTIONS", "POST", "PUT", "PATCH", "DELETE"]
-
-      if array?(required_opts)
-        generate_requirements_docs(required_opts)
-      elsif required_opts == true
-        generate_requirements_docs(default_opts)
-      else
-        string = ""
-      end
-    end
-
-    def array?(required_opt)
-      required_opt.kind_of? Array
-    end
-
-    def generate_requirements_docs(opts)
-      string = "\n#{bullet(bold('Required'), 4)}: "
-      opts.each { |opt| string += "#{code(opt)} "}
-      "#{string}\n\n"
-    end
-
-    def output
-      self.docs += "# HYPERDRIVE API\n"
-      resources.each_value do |resource|
-        self.docs += header(resource.name)
-        self.docs += paragraph(resource.desc)
-        self.docs += header("Endpoint URL", 2)
-        self.docs += endpoint(resource.endpoint)
-        self.docs += header("Params", 2)
-        self.docs += params(resource.allowed_params)
-        self.docs += header("Filter", 2)
-        self.docs += params(resource.filters)
-      end
-      self.docs
-    end
-
   end
 end
--- spec/hyperdrive/docs_spec.rb
+++ spec/hyperdrive/docs_spec.rb
@@ -9,7 +9,7 @@ describe Hyperdrive::Docs do
   end
 
   it 'generates a header with size 1 as default' do
-    @docs.header('Thing Resource').must_equal "\n# Thing Resource\n\n"
+    @docs.header('Thing Resource').must_equal "\n\n# Thing Resource\n\n"
   end
 
   it 'generates a header only between size 1 and 6' do
@@ -18,7 +18,7 @@ describe Hyperdrive::Docs do
   end
 
   it 'generates a paragraph' do
-    @docs.paragraph('Description of Thing Resource').must_equal "Description of Thing Resource\n"
+    @docs.paragraph('Description of Thing Resource').must_equal "Description of Thing Resource\n\n"
   end 
 
   it 'generates bold text' do
@@ -29,41 +29,25 @@ describe Hyperdrive::Docs do
     @docs.code('/things').must_equal '`/things`'
   end
 
-  it 'generates a bullet with nest level of 0 as default' do
-    @docs.bullet('test').must_equal '- test'
+  it 'generates a bullet with nest level of 1 as default' do
+    @docs.bullet('test').must_equal "\n  - test\n"
+  end
+
+  it 'generates a bullet with nest level between 1 and 3' do
+    proc {@docs.bullet('test', 4)}.must_raise ArgumentError
+    proc {@docs.bullet('test', 0)}.must_raise ArgumentError
   end
 
   it 'generates a nested bulleted list' do
-    @docs.bullet('test', 2).must_equal '  - test'
+    @docs.bullet('test', 2).must_equal "\n    - test\n"
   end
 
   it 'generates a nested bullet code span' do
-    @docs.bullet('`/things`', 2).must_equal '  - `/things`'
+    @docs.bullet('`/things`', 2).must_equal "\n    - `/things`\n"
   end
 
   it 'generates nested bulleted bold text' do
-    @docs.bullet('__id__', 4).must_equal '    - __id__'
-  end
-
-  it 'formats endpoint' do
-    @docs.endpoint('/thing').must_equal "  - `/thing`\n"
-  end
-
-  it 'formats param names' do
-    @docs.param_name('id').must_equal '  - __id__: '
-  end
-
-  it 'formats param descriptions' do
-    @docs.param_desc('Resource Identifier').must_equal "Resource Identifier\n"
-  end
-
-  it 'formats param requirements for default actions' do
-    @docs.required(true).must_equal "\n    - __Required__: `GET` `HEAD` `OPTIONS` `POST` `PUT` `PATCH` `DELETE` \n\n"
-  end
-
-  it 'generates param' do
-    params = @docs.resources[:thing].allowed_params
-    @docs.params(params).must_equal "  - __id__: Resource Identifier\n\n    - __Required__: `PUT` `PATCH` `DELETE` \n\n  - __name__: 50 Chars or less\n\n    - __Required__: `GET` `HEAD` `OPTIONS` `POST` `PUT` `PATCH` `DELETE` \n\n  - __start_date__: Format: YYYY-MM-DD\n  - __end_date__: Format: YYYY-MM-DD\n" 
+    @docs.bullet('__id__', 3).must_equal "\n      - __id__\n"
   end
 
   it 'outputs a string of the completed doc' do

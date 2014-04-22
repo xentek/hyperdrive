ca4637ea302cc626f2e3e9cb2c56c71e31e96719
Eric Marden <eric@xentek.net>
2014-04-22 16:21:00 -0500

    removes coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,13 +5,6 @@ ENV['RACK_ENV'] = 'test'
 lib_path = File.expand_path('../lib', __FILE__)
 ($:.unshift lib_path) unless ($:.include? lib_path)
 
-# test coverage
-if ENV['TRAVIS']
-  require 'coveralls'
-  require 'rubinius/coverage' if RUBY_ENGINE == 'rubinius'
-  Coveralls.wear!
-end
-
 # require dependencies
 require 'bundler'
 Bundler.setup(:default, ENV['RACK_ENV'])

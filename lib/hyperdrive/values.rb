a93ea5be9dbfb057dd87e8c25145827932ea8d58
Eric Marden <eric@xentek.net>
2014-04-22 04:22:18 -0500

    adds methods to map symbols to their HTTP equivalent and vice versa

--- lib/hyperdrive/values.rb
+++ lib/hyperdrive/values.rb
@@ -3,5 +3,29 @@ module Hyperdrive
     def self.request_methods
       %w(GET HEAD OPTIONS POST PUT PATCH DELETE).freeze
     end
+
+    def self.request_methods_symbol_map
+      {
+        get:     'GET',
+        head:    'HEAD',
+        options: 'OPTIONS',
+        post:    'POST',
+        put:     'PUT',
+        patch:   'PATCH',
+        delete:  'DELETE'
+      }.freeze
+    end
+
+    def self.request_methods_string_map
+      {
+        'GET'     => :get,
+        'HEAD'    => :head,
+        'OPTIONS' => :options,
+        'POST'    => :post,
+        'PUT'     => :put,
+        'PATCH'   => :patch,
+        'DELETE'  => :delete
+      }.freeze
+    end
   end
 end

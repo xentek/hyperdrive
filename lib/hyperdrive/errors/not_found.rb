19fbb608d2c1a62fe7e97d7a22111ceb9055060d
Justin Park <jpark3000@gmail.com>
2014-04-22 16:08:26 -0500

    makes message more concise

--- lib/hyperdrive/errors/not_found.rb
+++ lib/hyperdrive/errors/not_found.rb
@@ -2,7 +2,7 @@ module Hyperdrive
   module Errors
     class NotFound < HTTPError
       def message
-        "The requested resource could not be found but may be available again in the future."
+        "The requested resource could not be found."
       end
 
       def http_status_code

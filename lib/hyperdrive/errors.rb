a0c4b4fefb4da3a3f8a63365d599a82245d63075
Eric Marden <eric@xentek.net>
2014-04-22 04:22:11 -0500

    require new error classes

--- lib/hyperdrive/errors.rb
+++ lib/hyperdrive/errors.rb
@@ -1,3 +1,6 @@
 # encoding: utf-8
 
+require 'hyperdrive/errors/http_error'
+require 'hyperdrive/errors/method_not_allowed'
+require 'hyperdrive/errors/not_implemented'
 require 'hyperdrive/errors/dsl/unknown_argument'

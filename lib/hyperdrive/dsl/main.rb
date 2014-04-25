137ec7b2220af65404fd8116c9a0f772aae82250
Eric Marden <eric@xentek.net>
2014-04-24 22:39:33 -0500

    use implicit block captures
    this helps avoid unnecessary Proc creation where we can

--- lib/hyperdrive/dsl/main.rb
+++ lib/hyperdrive/dsl/main.rb
@@ -7,12 +7,12 @@ module Hyperdrive
       include Singleton
       attr_reader :resources
 
-      def initialize(&block)
+      def initialize
         @resources = {}
       end
 
-      def resource(name, &block)
-        @resources[name] = Resource.new(name, &block).resource
+      def resource(name)
+        @resources[name] = Resource.new(name, &Proc.new).resource
       end
     end
   end

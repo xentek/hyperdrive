e4e3597e8d70be822d23d83dd1c0c99f4622cf1e
Eric Marden <eric@xentek.net>
2014-04-27 22:10:44 -0500

    moves helper methods to Utils module

---
+++ lib/hyperdrive/utils.rb
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+module Hyperdrive
+  module Utils
+    def self.sanitize_keys(keys_to_keep, hash)
+      Hash[hash.select do |key, value|
+        keys_to_keep.include? key
+      end]
+    end
+
+    def self.symbolize_keys(hash)
+      hash.inject({}) do |result, (key, value)|
+       result.merge!(Hash[
+          case key
+          when String then key.to_sym
+          else key
+          end,
+          case value
+          when Hash then symbolize_keys(value)
+          when Array then value.map! { |v| v.is_a?(Hash) ? symbolize_keys(v) : v } ; value
+          else value
+          end
+        ])
+      end
+    end
+  end
+end
---
+++ spec/hyperdrive/utils_spec.rb
@@ -0,0 +1,38 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe Hyperdrive::Utils do
+  context '.sanitize_keys' do
+    before do
+      @subject = Hyperdrive::Utils.sanitize_keys([:keep], { keep: 'this', remove: 'that' })
+    end
+
+    it "removes keys that were specified from the given hash" do
+      @subject.key?(:remove).must_equal false
+    end
+
+    it "keeps keys that were not specified from the given hash" do
+      @subject.key?(:keep).must_equal true
+    end
+  end
+
+  context '.symbolize_keys' do
+    before do
+      @hash = { 'string' => 'cheese', 'collection' => [{'skylanders' => 155}], 'map' => { 'oceans' => 'blue' } }
+      @subject = Hyperdrive::Utils.symbolize_keys(@hash)
+    end
+
+    it "can symbolize the keys of a hash" do
+      @subject[:string].must_equal 'cheese'
+    end
+    
+    it "can symbolize the keys of nested hashes" do
+      @subject[:map][:oceans].must_equal 'blue'
+    end
+
+    it "can symbolize the keys of arrays of hashes" do
+      @subject[:collection].first[:skylanders].must_equal 155
+    end    
+  end
+end

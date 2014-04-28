03c244be611de9279b57ddfa5ff8b0f87aeeb461
Eric Marden <eric@xentek.net>
2014-04-28 12:44:05 -0500

    stubs out basic functionality for cors middleware

--- lib/hyperdrive/middleware/cors.rb
+++ lib/hyperdrive/middleware/cors.rb
@@ -9,19 +9,28 @@ module Hyperdrive
 
       def call(env)
         status, headers, body = @app.call(env)
+        #headers.merge!(cross_origin_headers)
         [status, headers, body]
       end
 
-      def cross_origin_headers
+      private
+
+      def default_cors_options
         {
-          'Access-Control-Allow-Origin' => origin,
-          'Access-Control-Allow-Methods' => methods,
-          'Access-Control-Allow-Headers' => settings.allow_headers,
-          'Access-Control-Allow-Credentials' => settings.allow_credentials.to_s,
-          'Access-Control-Max-Age' => settings.max_age.to_s,
-          'Access-Control-Expose-Headers' => settings.expose_headers.join(', ')
+          
         }
       end
+
+      #def cross_origin_headers
+        #{
+          #'Access-Control-Allow-Origin' => origin,
+          #'Access-Control-Allow-Methods' => methods,
+          #'Access-Control-Allow-Headers' => settings.allow_headers,
+          #'Access-Control-Allow-Credentials' => settings.allow_credentials.to_s,
+          #'Access-Control-Max-Age' => settings.max_age.to_s,
+          #'Access-Control-Expose-Headers' => settings.expose_headers.join(', ')
+        #}
+      #end
     end
   end
 end

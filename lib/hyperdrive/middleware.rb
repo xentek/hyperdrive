23a706017822a419cc7089626b340201958ed477
Eric Marden <eric@xentek.net>
2014-04-28 12:41:44 -0500

    requires cors middleware

--- lib/hyperdrive/middleware.rb
+++ lib/hyperdrive/middleware.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 
+require 'hyperdrive/middleware/cors'
 require 'hyperdrive/middleware/resource'
 require 'hyperdrive/middleware/sanitize_params'
 require 'hyperdrive/middleware/required_params'

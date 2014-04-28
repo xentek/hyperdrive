c1dc02471e5b7d3cf15da111073f85413cd954d5
Eric Marden <eric@xentek.net>
2014-04-27 22:36:07 -0500

    fix test isolation issue

--- spec/hyperdrive/middleware/resource_spec.rb
+++ spec/hyperdrive/middleware/resource_spec.rb
@@ -2,21 +2,16 @@
 
 require 'spec_helper'
 
-def app
-  Rack::Builder.new do
-    map '/' do
-      @resource = sample_api.resources.values.first
-      use Hyperdrive::Middleware::Resource, @resource
-      run ->(env) {
-        [200, {'Content-Type' => 'text/plain'}, [env['hyperdrive.resource'].name]]
-      }
-    end
-  end
-end
-
 describe Hyperdrive::Middleware::Resource do
+  def app
+    inner_app = lambda { |env|
+      [200, {'Content-Type' => 'text/plain'}, [env['hyperdrive.resource'].name]]
+    }
+    Hyperdrive::Middleware::Resource.new(inner_app, sample_api.resources.values.first)
+  end
+
   it "adds the requested resource to rack's env" do
-    get '/'
+    get '/resource'
     last_response.body.must_equal "Thing Resource"
   end
 end
--- spec/hyperdrive/middleware/sanitize_params_spec.rb
+++ spec/hyperdrive/middleware/sanitize_params_spec.rb
@@ -2,20 +2,20 @@
 
 require 'spec_helper'
 
-def app
-  Rack::Builder.new do
-    map '/' do
-      @resource = sample_api.resources.values.first
-      use Hyperdrive::Middleware::Resource, @resource
-      use Hyperdrive::Middleware::SanitizeParams
-      run ->(env) {
-        [200, {'Content-Type' => 'text/plain'}, [env['hyperdrive.params']]]
-      }
+describe Hyperdrive::Middleware::SanitizeParams do
+  def app
+    Rack::Builder.new do
+      map '/' do
+        @resource = sample_api.resources.values.first
+        use Hyperdrive::Middleware::Resource, @resource
+        use Hyperdrive::Middleware::SanitizeParams
+        run ->(env) {
+          [200, {'Content-Type' => 'text/plain'}, [env['hyperdrive.params']]]
+        }
+      end
     end
   end
-end
 
-describe Hyperdrive::Middleware::SanitizeParams do
   context "GET" do
     before do
       get '/', { 'id' => '1001', 'removed' => 'me' }

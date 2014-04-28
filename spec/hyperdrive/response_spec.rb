45c88d6912c4d664bdb05cd0e29d5159f94a6b14
Eric Marden <eric@xentek.net>
2014-04-27 22:22:38 -0500

    cleans up spec after recent changes

--- spec/hyperdrive/response_spec.rb
+++ spec/hyperdrive/response_spec.rb
@@ -5,19 +5,22 @@ require 'spec_helper'
 describe Hyperdrive::Response do
   before do
     @resource = Hyperdrive::Resource.new(:things)
+    @env = default_rack_env.merge('hyperdrive.resource' => @resource)
   end
 
   it "throws an error when the request method is unspported by hyperdrive" do
-    proc { Hyperdrive::Response.new({ 'REQUEST_METHOD' => 'TRACE' }, @resource) }.must_raise Hyperdrive::Errors::NotImplemented
+    @env = @env.merge('REQUEST_METHOD' => 'TRACE')
+    proc { Hyperdrive::Response.new(@env) }.must_raise Hyperdrive::Errors::NotImplemented
   end
 
   it "throws an error when the request method is not allowed" do
-    proc { Hyperdrive::Response.new({ 'REQUEST_METHOD' => 'GET' }, @resource) }.must_raise Hyperdrive::Errors::MethodNotAllowed
+    proc { Hyperdrive::Response.new(@env) }.must_raise Hyperdrive::Errors::MethodNotAllowed
   end
 
   it "responds to requests" do
     @resource.register_request_handler(:get, Proc.new {|env| 'ok' })
-    response = Hyperdrive::Response.new({ 'REQUEST_METHOD' => 'GET' }, @resource)
+    @env = @env.merge('hyperdrive.resource' => @resource)
+    response = Hyperdrive::Response.new(@env)
     response.response.first.must_equal 200
   end
 end

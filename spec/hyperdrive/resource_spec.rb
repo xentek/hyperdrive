f8da689daabd4230fc2f8b6ba3f86d10a1d81c29
Eric Marden <eric@xentek.net>
2014-04-28 12:44:49 -0500

    w/s

--- spec/hyperdrive/resource_spec.rb
+++ spec/hyperdrive/resource_spec.rb
@@ -32,13 +32,13 @@ describe Hyperdrive::Resource do
 
   it "auto-registers the :id filter" do
     @resource.filters[:id][:desc].must_equal 'Resource Identifier'
-    @resource.filters[:id][:required].must_equal [] 
+    @resource.filters[:id][:required].must_equal []
   end
 
   it "registers a filter" do
     @resource.register_filter(:parent_id, 'Parent ID of Thing', required: true)
     @resource.filters[:parent_id][:desc].must_equal 'Parent ID of Thing'
-    @resource.filters[:parent_id][:required].must_equal %w(GET HEAD) 
+    @resource.filters[:parent_id][:required].must_equal %w(GET HEAD)
   end
 
   context "Request Handlers" do

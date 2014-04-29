73399988fdc0e56cff0fc724dc40907b987602e3
Eric Marden <eric@xentek.net>
2014-04-29 13:48:09 -0500

    create new resource explicitly to fix test isolation issues

--- spec/hyperdrive/middleware/resource_spec.rb
+++ spec/hyperdrive/middleware/resource_spec.rb
@@ -3,15 +3,19 @@
 require 'spec_helper'
 
 describe Hyperdrive::Middleware::Resource do
+  before do
+    @resource = Hyperdrive::Resource.new(:thing)
+  end
+
   def app
     inner_app = lambda { |env|
-      [200, {'Content-Type' => 'text/plain'}, [env['hyperdrive.resource'].name]]
+      [200, {'Content-Type' => 'text/plain'}, [env['hyperdrive.resource'].namespace]]
     }
-    Hyperdrive::Middleware::Resource.new(inner_app, sample_api.resources.values.first)
+    Hyperdrive::Middleware::Resource.new(inner_app, @resource)
   end
 
   it "adds the requested resource to rack's env" do
     get '/resource'
-    last_response.body.must_equal "Thing Resource"
+    last_response.body.must_equal "things"
   end
 end

fe084c9df646803f1032682c66a3e345fb098b8b
Justin Park <jpark3000@gmail.com>
2014-04-28 19:05:54 -0500

    adds default CORS options, adds support for custom CORS options

--- lib/hyperdrive/middleware/cors.rb
+++ lib/hyperdrive/middleware/cors.rb
@@ -5,32 +5,39 @@ module Hyperdrive
     class CORS
       def initialize(app, options = {})
         @app = app
+        @options = options
       end
 
       def call(env)
         status, headers, body = @app.call(env)
-        #headers.merge!(cross_origin_headers)
+        options = default_cors_options(env).merge!(@options)
+        headers.merge!(options)
         [status, headers, body]
       end
 
       private
 
-      def default_cors_options
+      def default_cors_options(env)
         {
-          
+          'Access-Control-Allow-Origin' => '*',
+          'Access-Control-Allow-Methods' => env['hyperdrive.resource'].allowed_methods.join(", "),
+          'Access-Control-Allow-Headers' => "*, Content-Type, Accept, AUTHORIZATION, Cache-Control",
+          'Access-Control-Allow-Credentials' => "true",
+          'Access-Control-Max-Age' => 1728000,
+          'Access-Control-Expose-Headers' => "Cache-Control, Content-Language, Content-Type, Expires, Last-Modified, Pragma"
         }
       end
 
-      #def cross_origin_headers
-        #{
-          #'Access-Control-Allow-Origin' => origin,
-          #'Access-Control-Allow-Methods' => methods,
-          #'Access-Control-Allow-Headers' => settings.allow_headers,
-          #'Access-Control-Allow-Credentials' => settings.allow_credentials.to_s,
-          #'Access-Control-Max-Age' => settings.max_age.to_s,
-          #'Access-Control-Expose-Headers' => settings.expose_headers.join(', ')
-        #}
-      #end
+      # def cross_origin_headers
+      #   {
+      #     'Access-Control-Allow-Origin' => origin,
+      #     'Access-Control-Allow-Methods' => methods,
+      #     'Access-Control-Allow-Headers' => settings.allow_headers,
+      #     'Access-Control-Allow-Credentials' => settings.allow_credentials.to_s,
+      #     'Access-Control-Max-Age' => settings.max_age.to_s,
+      #     'Access-Control-Expose-Headers' => settings.expose_headers.join(', ')
+      #   }
+      # end
     end
   end
 end

b6aea0dabd47b7b13707a9af314064000f1a1bb9
Justin Park <jpark3000@gmail.com>
2014-04-28 19:06:46 -0500

    adds testing for CORS configuration

--- spec/hyperdrive/dsl/main_spec.rb
+++ spec/hyperdrive/dsl/main_spec.rb
@@ -13,4 +13,19 @@ describe Hyperdrive::DSL::Main do
       resource(:thing) {}
     end.resources[:thing].must_be_instance_of ::Hyperdrive::Resource
   end
+
+  it "keeps required CORS headers" do
+    hyperdrive do
+      cors(origins: '*', headers: ['Content-Type', 'Origin', 'Accept'], credentials: true, expose: ['test'], max_age: 1728000)
+    end.config[:cors].keys.must_equal [:origins, :headers, :credentials, :expose, :max_age]
+
+  end
+
+  it "strips off unallowed CORS headers" do
+    hyperdrive do
+      cors(origins: '*', headers: ['Content-Type', 'Origin', 'Accept'], 
+           credentials: true, expose: ['test'], test: 'test', another_test: 'test')
+    end.config[:cors].keys.must_equal [:origins, :headers, :credentials, :expose]
+
+  end
 end

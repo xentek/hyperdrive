6ac1c99b7ac9a9c7fa0d839e125a05b89ceb0c00
Eric Marden <eric@xentek.net>
2014-04-29 04:30:00 -0500

    requires content negotiation middleware

--- lib/hyperdrive/middleware.rb
+++ lib/hyperdrive/middleware.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 require 'hyperdrive/middleware/cors'
+require 'hyperdrive/middleware/content_negotiation'
 require 'hyperdrive/middleware/resource'
 require 'hyperdrive/middleware/sanitize_params'
 require 'hyperdrive/middleware/required_params'

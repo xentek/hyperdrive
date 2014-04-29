dd0d3b1ba766d88f9fbcb8e582cc7d6c8e22ef16
Eric Marden <eric@xentek.net>
2014-04-29 03:18:41 -0500

    adds vendor and media_types config to dsl
    will use these to construct Content-Type(s)

--- lib/hyperdrive/dsl/main.rb
+++ lib/hyperdrive/dsl/main.rb
@@ -5,17 +5,33 @@ module Hyperdrive
   module DSL
     class Main
       include Singleton
-      attr_reader :resources
+      attr_reader :resources, :config
 
       def initialize
         @resources = {}
+        @config = default_config
       end
 
       def cors(options = {})
       end
 
+      def vendor(vendor)
+        @config[:vendor] = vendor
+      end
+
+      def media_types(media_types)
+        @config[:media_types] = media_types
+      end
+
       def resource(name)
         @resources[name] = Resource.new(name, &Proc.new).resource
+      end
+
+      def default_config
+        {
+          vendor: 'hyperdrive',
+          media_types: %w(hal+json json)
+        }
       end
     end
   end
--- spec/hyperdrive/dsl/main_spec.rb
+++ spec/hyperdrive/dsl/main_spec.rb
@@ -8,6 +8,18 @@ describe Hyperdrive::DSL::Main do
     end.must_be_instance_of ::Hyperdrive::DSL::Main
   end
 
+  it "has a vendor" do
+    hyperdrive do
+      vendor 'planet-express'
+    end.config[:vendor].must_equal 'planet-express'
+  end
+
+  it "has media types" do
+    hyperdrive do
+      media_types ['json']
+    end.config[:media_types].must_equal ['json']
+  end
+
   it "registers a resource" do
     hyperdrive do
       resource(:thing) {}

3c531aa1798ec3ff25b7590786a5a9a49342d729
Justin Park <jpark3000@gmail.com>
2014-04-28 19:07:11 -0500

    adds testing for CORS middleware

--- spec/hyperdrive/middleware/cors_spec.rb
+++ spec/hyperdrive/middleware/cors_spec.rb
@@ -11,30 +11,72 @@ describe Hyperdrive::Middleware::CORS do
   end
 
   before do
-    get '/'
+    hyperdrive_env = {
+      'hyperdrive.resource' => sample_api.resources.values.first
+    }
+    env = default_rack_env.merge(hyperdrive_env)
+    status, @headers, body = app.call(env)
   end
 
-  it "allows origins" do
-    last_response.header['Access-Control-Allow-Origin'].must_equal ''
+  context 'Default CORS options' do
+    it "allows origins" do
+      @headers['Access-Control-Allow-Origin'].must_equal '*'
+    end
+
+    it "allows methods" do
+      @headers['Access-Control-Allow-Methods'].must_equal 'OPTIONS'
+    end
+
+    it "allows headers" do
+      @headers['Access-Control-Allow-Headers'].must_equal '*, Content-Type, Accept, AUTHORIZATION, Cache-Control'
+    end
+
+    it "allows credentials" do
+      @headers['Access-Control-Allow-Credentials'].must_equal "true"
+    end
+
+    it "has a max age" do
+      @headers['Access-Control-Max-Age'].must_equal 1728000
+    end
+
+    it "exposes headers" do
+      @headers['Access-Control-Expose-Headers'].must_equal 'Cache-Control, Content-Language, Content-Type, Expires, Last-Modified, Pragma'
+    end
   end
 
-  it "allows methods" do
-    last_response.header['Access-Control-Allow-Methods'].must_equal ''
-  end
+  context 'Custom CORS options' do
+    def app
+      inner_app = lambda { |env|
+        [200, {'Content-Type' => 'text/plain'}, ['cors okay']]
+      }
+      options = {'Access-Control-Allow-Credentials' => 'false',
+                 'Access-Control-Allow-Origin' => 'http://example.com',
+                 'Access-Control-Max-Age' => 123,
+                 'Access-Control-Expose-Headers' => 'Cache-Control, Content-Language',
+                 'Access-Control-Allow-Headers' => '*, Content-Type'}
+      Hyperdrive::Middleware::CORS.new(inner_app, options) 
+    end
 
-  it "allows headers" do
-    last_response.header['Access-Control-Allow-Headers'].must_equal ''
-  end
+    it "sets custom Origin" do
+      @headers['Access-Control-Allow-Origin'].must_equal 'http://example.com'
+    end
 
-  it "allows credentials" do
-    last_response.header['Access-Control-Allow-Credentials'].must_equal ''
-  end
+    it "sets custom Credentials" do
+      @headers['Access-Control-Allow-Credentials'].must_equal 'false'
+    end
 
-  it "has a max age" do
-    last_response.header['Access-Control-Max-Age'].must_equal ''
-  end
+    it "sets custom Max-Age" do
+      @headers['Access-Control-Max-Age'].must_equal 123
+    end
 
-  it "exposes headers" do
-    last_response.header['Access-Control-Expose-Headers'].must_equal ''
+    it "sets custom Expose-Headers" do
+      @headers['Access-Control-Expose-Headers'].must_equal 'Cache-Control, Content-Language'
+    end
+
+    it "sets custom Headers" do
+      @headers['Access-Control-Allow-Headers'].must_equal '*, Content-Type'
+    end
+
+
   end
 end

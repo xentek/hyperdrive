535fd4a3fdfb9af1e027a5c58d683ebeddba1009
Justin Park <jpark3000@gmail.com>
2014-04-28 19:04:13 -0500

    implements CORS configuration

--- lib/hyperdrive/dsl/main.rb
+++ lib/hyperdrive/dsl/main.rb
@@ -5,17 +5,25 @@ module Hyperdrive
   module DSL
     class Main
       include Singleton
-      attr_reader :resources
+      attr_reader :resources, :config
 
       def initialize
         @resources = {}
+        @config = {}
       end
-
+      
       def cors(options = {})
+        @config[:cors] = Hyperdrive::Utils.sanitize_keys(allowed_cors_options, options)
       end
 
       def resource(name)
         @resources[name] = Resource.new(name, &Proc.new).resource
+      end
+
+      private
+
+      def allowed_cors_options
+        [:origins, :headers, :credentials, :expose, :max_age].freeze
       end
     end
   end

246ba3ecf6a9d4cb1c99543f7438d12da5124227
Eric Marden <eric@xentek.net>
2014-04-29 13:46:13 -0500

    commenting out specs to debug test isolation issue

--- spec/hyperdrive/dsl/main_spec.rb
+++ spec/hyperdrive/dsl/main_spec.rb
@@ -3,26 +3,26 @@
 require 'spec_helper'
 
 describe Hyperdrive::DSL::Main do
-  it "creates a singleton instance of hyperdrive" do
-    hyperdrive do
-    end.must_be_instance_of ::Hyperdrive::DSL::Main
-  end
+  #it "creates a singleton instance of hyperdrive" do
+    #hyperdrive do
+    #end.must_be_instance_of ::Hyperdrive::DSL::Main
+  #end
 
-  it "has a vendor" do
-    hyperdrive do
-      vendor 'planet-express'
-    end.config[:vendor].must_equal 'planet-express'
-  end
+  #it "has a vendor" do
+    #hyperdrive do
+      #vendor 'planet-express'
+    #end.config[:vendor].must_equal 'planet-express'
+  #end
 
-  it "has media types" do
-    hyperdrive do
-      media_types ['json']
-    end.config[:media_types].must_equal ['json']
-  end
+  #it "has media types" do
+    #hyperdrive do
+      #media_types ['json']
+    #end.config[:media_types].must_equal ['json']
+  #end
 
-  it "registers a resource" do
-    hyperdrive do
-      resource(:thing) {}
-    end.resources[:thing].must_be_instance_of ::Hyperdrive::Resource
-  end
+  #it "registers a resource" do
+    #hyperdrive do
+      #resource(:thing) {}
+    #end.resources[:thing].must_be_instance_of ::Hyperdrive::Resource
+  #end
 end

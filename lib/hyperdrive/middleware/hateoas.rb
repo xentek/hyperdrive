da798865846a2bce372d4bb413638677fdc325ee
Eric Marden <eric@xentek.net>
2014-05-02 16:59:16 -0500

    integrates new hateoas endpoint into server

--- lib/hyperdrive/middleware/hateoas.rb
+++ lib/hyperdrive/middleware/hateoas.rb
@@ -13,27 +13,25 @@ module Hyperdrive
         raise Hyperdrive::Errors::NotFound if resources.empty?
         status, headers, body = @app.call(env)
 
-        headers['Allow'] = Hyperdrive::Values.supported_request_methods.join(",")
-        
         endpoints = resources.map do |_,resource|
           resource.to_hash
         end
-        
-        media_types = %w(application/json application/xml)
+
+        media_types = %w(application/json)
         content_type = env['hyperdrive.accept'].best_of(media_types)
-        headers['Content-Type'] = content_type
-        body = case content_type 
+        body = case content_type
                when 'application/json'
                  Oj.dump(endpoints, mode: :compat)
                when 'application/xml'
                  Ox.dump(endpoints)
                else
-                 nil
+                 raise Hyperdrive::Errors::NotAcceptable.new(env['HTTP_ACCEPT'])
                end
-        
-        raise Hyperdrive::Errors::NotAcceptable.new(env['HTTP_ACCEPT']) if body.nil?
- 
-        [status, headers, body]
+
+        status = 200
+        headers['Allow'] = Hyperdrive::Values.supported_request_methods.join(",")
+        headers['Content-Type'] = content_type
+        [status, headers, [body]]
       end
     end
   end
--- lib/hyperdrive/server.rb
+++ lib/hyperdrive/server.rb
@@ -13,33 +13,32 @@ module Hyperdrive
         use Rack::Runtime
         use Rack::Lint
         use Rack::Head
-
+        use Hyperdrive::Middleware::Accept
         map '/' do
-          info = ''
-          hyperdrive.resources.each do |type, resource|
-            info += %Q({"id":"#{resource.endpoint}","name":"#{resource.name}","desc":"#{resource.desc}","type":"#{type}"})
-          end
-
-          run ->(env) {
-            [200, { 'Content-Type' => 'application/json', 'Allow' => Hyperdrive::Values.supported_request_methods.join(",") }, ["[#{info}]"]]
-          }
-        end
-
-        hyperdrive.resources.each do |key, resource|
-          use Hyperdrive::Middleware::Resource, resource
-          use Hyperdrive::Middleware::CORS, hyperdrive.config[:cors]
-          use Hyperdrive::Middleware::SanitizeParams
-          use Hyperdrive::Middleware::RequiredParams
-          map resource.endpoint do
-            run ->(env) {
-              begin
-                Hyperdrive::Response.new(env).response
-              rescue Hyperdrive::Errors::HTTPError => error
-                [error.http_status_code, { 'Allow' => resource.allowed_methods.join(',') }, [error.message]]
-              end
-            }
+          begin
+            inner_app = ->(env) { [404, {}, ['']] }
+            run Hyperdrive::Middleware::HATEOAS.new inner_app, hyperdrive.resources
+          rescue Hyperdrive::Errors::HTTPError => error
+            [error.http_status_code, { 'Allow' => 'GET' }, [error.message]]
           end
         end
+
+        #hyperdrive.resources.each do |key, resource|
+          #use Hyperdrive::Middleware::Resource, resource
+          #use Hyperdrive::Middleware::RequestMethod
+          #use Hyperdrive::Middleware::CORS, hyperdrive.config[:cors]
+          #use Hyperdrive::Middleware::SanitizeParams
+          #use Hyperdrive::Middleware::RequiredParams
+          #map resource.endpoint do
+            #run ->(env) {
+              #begin
+                #Hyperdrive::Response.new(env).response
+              #rescue Hyperdrive::Errors::HTTPError => error
+                #[error.http_status_code, { 'Allow' => resource.allowed_methods.join(',') }, [error.message]]
+              #end
+            #}
+          #end
+        #end
       end.to_app
     end
   end

ee67f9697df87a88352e266388b97711022c6361
Eric Marden <eric@xentek.net>
2014-05-03 20:59:34 -0500

    cleans up HATEOAS and makes it a rack app
      - no longer has initialize method that takes an app
      - extended with Values object
      - gets resources via hyperdrive.resources (so that we can use it as an
        App and not a Middleware)
      - returns some basic cors headers manually
      - throws 404 if PATH_INFO '/' (which ensures we don't return discovery
        info when no other path matches)
      - only returns JSON now (Ox did not serialize ruby hash sanely)
      - will set content type to hal+json if that was in the request,
        otherwise it will default to plain json (hal conventions still used
        in either case though)
      - returns Allow header set to only GET, HEAD, OPTIONS which more
        closely follow the spec

--- lib/hyperdrive/middleware/hateoas.rb
+++ lib/hyperdrive/middleware/hateoas.rb
@@ -3,33 +3,38 @@
 module Hyperdrive
   module Middleware
     class HATEOAS
-      attr_reader :resources
-      def initialize(app, resources = {})
-        @app = app
-        @resources = resources
-      end
+      extend Hyperdrive::Values
 
-      def call(env)
-        raise Hyperdrive::Errors::NotFound if resources.empty?
-        status, headers, body = @app.call(env)
+      def self.call(env)
+        if hyperdrive.resources.empty? || env['PATH_INFO'] != '/'
+          raise Hyperdrive::Errors::NotFound
+        end
 
-        endpoints = resources.map do |_,resource|
+        endpoints = hyperdrive.resources.map do |_,resource|
           resource.to_hash
         end
 
-        media_types = %w(application/json)
+        api = {
+                _links: { self: { href: '/' } },
+                name: hyperdrive.config[:name],
+                description: hyperdrive.config[:description],
+                vendor: hyperdrive.config[:vendor],
+                resources: endpoints
+              }
+
+        media_types = %w(application/hal+json application/json)
         content_type = env['hyperdrive.accept'].best_of(media_types)
-        body = case content_type
-               when 'application/json'
-                 Oj.dump(endpoints, mode: :compat)
-               when 'application/xml'
-                 Ox.dump(endpoints)
+        body = if content_type =~ /json$/
+                 Oj.dump(api, mode: :compat)
                else
                  raise Hyperdrive::Errors::NotAcceptable.new(env['HTTP_ACCEPT'])
                end
 
         status = 200
-        headers['Allow'] = Hyperdrive::Values.supported_request_methods.join(",")
+        headers = {}
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Methods'] = 'GET, HEAD, OPTIONS' 
+        headers['Allow'] = 'GET, HEAD, OPTIONS' 
         headers['Content-Type'] = content_type
         [status, headers, [body]]
       end

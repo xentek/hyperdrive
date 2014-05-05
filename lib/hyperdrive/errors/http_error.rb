0addff8e3fcfcde1b70175d5afbfedb0f7fdd41b
Eric Marden <eric@xentek.net>
2014-05-05 16:02:55 -0500

    adds error message

--- lib/hyperdrive/errors/http_error.rb
+++ lib/hyperdrive/errors/http_error.rb
@@ -3,6 +3,10 @@
 module Hyperdrive
   module Errors 
     class HTTPError < RuntimeError
+      def message
+        'Unknown Error.'
+      end
+
       def http_status_code
         500
       end

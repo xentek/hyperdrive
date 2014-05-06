677a58d4558465487b69669c015004567c81927c
Eric Marden <eric@xentek.net>
2014-05-06 14:34:55 -0500

    use attr_reader's instead of manual method definition
    - update example to use 418 status (more semantic as example error)
    - change order of arguments so we can set a default status code of 500
    
    refs #9

--- lib/hyperdrive/errors/http_error.rb
+++ lib/hyperdrive/errors/http_error.rb
@@ -3,18 +3,10 @@
 module Hyperdrive
   module Errors 
     class HTTPError < RuntimeError
-
-      def initialize(http_status_code, message)
-        @http_status_code = http_status_code
+      attr_reader :http_status_code, :message
+      def initialize(message, http_status_code = 500)
         @message = message
-      end
-
-      def message
-        @message
-      end
-
-      def http_status_code
-        @http_status_code
+        @http_status_code = http_status_code
       end
     end
   end
--- spec/hyperdrive/errors/http_error_spec.rb
+++ spec/hyperdrive/errors/http_error_spec.rb
@@ -4,15 +4,15 @@ require 'spec_helper'
 
 describe Hyperdrive::Errors::HTTPError do
   before do
-    @error = Hyperdrive::Errors::HTTPError.new(500, 'EXAMPLE ERROR')
+    @error = Hyperdrive::Errors::HTTPError.new("I'M A TEAPOT", 418)
   end
 
   it "returns a 500 status code" do
-    @error.http_status_code.must_equal 500
+    @error.http_status_code.must_equal 418
   end
 
   it "has a message" do
-    @error.message.must_equal 'EXAMPLE ERROR'
+    @error.message.must_match(/TEAPOT/)
   end
 end
 

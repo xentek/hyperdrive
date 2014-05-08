e0702e80fbe4db3e5b342e80404d7e039629e272
Eric Marden <eric@xentek.net>
2014-05-08 16:56:48 -0500

    fix issues with setting pagination values
    this now runs before sanitize params and reads page/per_page out of
    request.params instead of hyperdrive.params

--- lib/hyperdrive/middleware/pagination.rb
+++ lib/hyperdrive/middleware/pagination.rb
@@ -3,16 +3,19 @@
 module Hyperdrive
   module Middleware
     class Pagination
+      attr_reader :default_per_page
       def initialize(app)
         @app = app
+        @default_per_page = hyperdrive.config.fetch(:per_page) { 20 }
       end
 
       def call(env)
         if %(GET HEAD).include? env['REQUEST_METHOD']
-          env['hyperdrive.page'] = env['hyperdrive.params'].delete(:page) { 1 }
-          env['hyperdrive.page'] = 1 if env['hyperdrive.page'].to_i < 1
-          env['hyperdrive.per_page'] = env['hyperdrive.params'].delete(:per_page) { hyperdrive.config.fetch(:per_page) { 20 } }
-          env['hyperdrive.per_page'] = 20 if env['hyperdrive.per_page'].to_i == 0
+          params = Rack::Request.new(env).params
+          env['hyperdrive.page']     = params.delete('page') { 1 }.to_i
+          env['hyperdrive.page']     = 1 if env['hyperdrive.page'] < 1
+          env['hyperdrive.per_page'] = params.delete('per_page') { default_per_page }.to_i
+          env['hyperdrive.per_page'] = default_per_page if env['hyperdrive.per_page'] == 0
         end
         @app.call(env)
       end
--- spec/hyperdrive/middleware/pagination_spec.rb
+++ spec/hyperdrive/middleware/pagination_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 describe Hyperdrive::Middleware::Pagination do
   def app
-    inner_app = ->(env) { [200, {}, [[env['hyperdrive.page'],env['hyperdrive.per_page'],env['hyperdrive.params'][:page],env['hyperdrive.params'][:per_page]].join(",")]] } 
+    inner_app = ->(env) { [200, {}, [[env['hyperdrive.page'],env['hyperdrive.per_page']].join(',')]] }
     Hyperdrive::Middleware::Pagination.new(inner_app)
   end
 
@@ -12,34 +12,34 @@ describe Hyperdrive::Middleware::Pagination do
     hyperdrive.send(:reset!)
   end
   
-  it "sets default values for page and per_page and removes them from hyperdrive.params" do
-    env = default_rack_env.merge('hyperdrive.params' => {})
-    get '/', {}, env
-    last_response.body.must_equal '1,20,,'
+  it "sets default values for page and per_page" do
+    get '/', {}
+    last_response.body.must_equal '1,20'
   end
 
   it "sets page to 1 if value is less than 1" do
-    env = default_rack_env.merge('hyperdrive.params' => { page: '' })
-    get '/', {}, env
-    last_response.body.must_equal '1,20,,'
+    get '/', { 'page' => '' }
+    last_response.body.must_equal '1,20'
+  end
+
+  it "returns page value if supplied" do
+    get '/', { 'page' => '2' }
+    last_response.body.must_equal '2,20'
   end
 
   it "returns per_page value if supplied" do
-    env = default_rack_env.merge('hyperdrive.params' => { per_page: 42 })
-    get '/', {}, env
-    last_response.body.must_equal '1,42,,'
+    get '/', { 'per_page' => 42 }
+    last_response.body.must_equal '1,42'
   end
 
   it "uses configured per_page value if per_page is not supplied" do
     hyperdrive.config[:per_page] = 13
-    env = default_rack_env.merge('hyperdrive.params' => {})
-    get '/', {}, env
-    last_response.body.must_equal '1,13,,'    
+    get '/', {}
+    last_response.body.must_equal '1,13'    
   end
 
   it "sets per_page to 20 if value is 0" do
-    env = default_rack_env.merge('hyperdrive.params' => { per_page: '' })
-    get '/', {}, env
-    last_response.body.must_equal '1,20,,'
+    get '/', { 'per_page' => '' }
+    last_response.body.must_equal '1,20'
   end
 end

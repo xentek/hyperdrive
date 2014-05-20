b10e591ca00361aea0ce7b60e557642482fbd712
Eric Marden <eric@xentek.net>
2014-05-20 02:13:05 -0500

    break up logic into methods

--- lib/hyperdrive/hateoas.rb
+++ lib/hyperdrive/hateoas.rb
@@ -5,41 +5,57 @@ module Hyperdrive
     extend Hyperdrive::Values
 
     def self.call(env)
+      @env = env
       if hyperdrive.resources.empty? || env['PATH_INFO'] != '/'
         raise Hyperdrive::Errors::NotFound
       end
 
-      endpoints = hyperdrive.resources.map do |_,resource|
+      [200, headers, [body]]
+    end
+
+    private
+  
+    def self.endpoints
+      hyperdrive.resources.map do |_,resource|
         resource.to_hash
       end
+    end
 
-      api = {
-              _links: { self: { href: '/' } },
-              name: hyperdrive.config[:name],
-              description: hyperdrive.config[:description],
-              vendor: hyperdrive.config[:vendor],
-              resources: endpoints
-            }
-
-      media_types = %w(hal+json json).map do |media_type|
-        "application/vnd.#{hyperdrive.config[:vendor]}+#{media_type}"
+    def self.response
+      {
+        _links: { self: { href: '/' } },
+        name: hyperdrive.config[:name],
+        description: hyperdrive.config[:description],
+        vendor: hyperdrive.config[:vendor],
+        resources: endpoints
+      }
+    end
+    
+    def self.body
+      if content_type =~ /json$/
+        MultiJson.dump(response)
+      else
+        raise Errors::NotAcceptable.new(@env['HTTP_ACCEPT'])
       end
+    end
 
-      media_types += %w(application/hal+json application/json)
-      content_type = env['hyperdrive.accept'].best_of(media_types)
-      body = if content_type =~ /json$/
-                MultiJson.dump(api)
-              else
-                raise Errors::NotAcceptable.new(env['HTTP_ACCEPT'])
-              end
+    def self.media_types
+      %w(hal+json json).map do |media_type|
+        "application/vnd.#{hyperdrive.config[:vendor]}+#{media_type}"
+      end + %w(application/hal+json application/json)
+    end
 
-      status = 200
-      headers = {}
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'GET, HEAD, OPTIONS'
-      headers['Allow'] = 'GET, HEAD, OPTIONS'
-      headers['Content-Type'] = content_type
-      [status, headers, [body]]
+    def self.content_type
+      @env['hyperdrive.accept'].best_of(media_types)
+    end
+
+    def self.headers
+      {
+        'Access-Control-Allow-Origin' => '*',
+        'Access-Control-Allow-Methods' => 'GET, HEAD, OPTIONS',
+        'Allow' => 'GET, HEAD, OPTIONS',
+        'Content-Type' => content_type
+      }
     end
   end
 end

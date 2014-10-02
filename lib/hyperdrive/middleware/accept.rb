68fa8a246efe5a01cc19ee25d7c7486eb6ef5f36
Justin Park <justin@mytastebud.com>
2014-10-02 16:04:13 -0500

    parses 'HTTP_ACCEPT_CHARSET' into env

--- lib/hyperdrive/middleware/accept.rb
+++ lib/hyperdrive/middleware/accept.rb
@@ -9,6 +9,7 @@ module Hyperdrive
 
       def call(env)
         env['hyperdrive.accept'] = Rack::Accept::MediaType.new(env['HTTP_ACCEPT'])
+        env['hyperdrive.accept_charset'] = Rack::Accept::Charset.new(env['HTTP_ACCEPT_CHARSET'])
         @app.call(env)
       end
     end

2b17e51c3c48ea8e62cb7ba312b56c46a82dbb9f
Justin Park <justin@mytastebud.com>
2014-10-03 13:47:53 -0500

    accounts for missing ACCEPT-CHARSET header

--- lib/hyperdrive/middleware/charset.rb
+++ lib/hyperdrive/middleware/charset.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
-
+require 'pry'
 module Hyperdrive
   module Middleware
     class Charset
@@ -9,7 +9,7 @@ module Hyperdrive
 
       def call(env)
         unless %w(OPTIONS TRACE).include? env['REQUEST_METHOD']
-          charset = Rack::Accept::Charset.new(env['HTTP_ACCEPT_CHARSET'])
+          charset = Rack::Accept::Charset.new(env['HTTP_ACCEPT_CHARSET'] || 'UTF-8')
           Hyperdrive::Utils.enforce_charset!(charset, env['hyperdrive.params'])
         end
         @app.call(env)
